2d7bf6cba266ad6c1cd6c63641c2c67a8fb25951
PythonKicks <pythonkicks@gmail.com>
2016-08-09 16:26:55 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import requests
-from Tkinter import *  # if you are using Python 3+ please change this line to from tkinter import *
+from Tkinter import *
 from threading import Thread
+import time
 
 
 class StockCheckerApp(object):
@@ -20,47 +21,124 @@ class StockCheckerApp(object):
         self.right_frame = Frame(master, width=width/2, height=height)
         self.right_frame.pack(side=RIGHT)
 
+        # sku label and input
+        self.sku_label = Label(self.left_frame, text='Enter SKU:')
+        self.sku_label.pack(anchor='w')
         self.sku_input = Entry(self.left_frame)
         self.sku_input.insert(0, 'SKU Here')
         self.sku_input.pack(anchor='w')
 
+        add_space(self.left_frame)
+
+        # client id label and input
+        self.client_id_label = Label(self.left_frame, text='Enter Client ID:')
+        self.client_id_label.pack(anchor='w')
         self.client_id_input = Entry(self.left_frame)
         self.client_id_input.insert(0, 'Client ID Here')
         self.client_id_input.pack(anchor='w')
 
+        add_space(self.left_frame)
+
+        # region label and input
+        self.region_label = Label(self.left_frame, text='Enter region:')
+        self.region_label.pack(anchor='w')
         self.region_input = Entry(self.left_frame)
         self.region_input.insert(0, 'Region Here')
         self.region_input.pack(anchor='w')
 
+        add_space(self.left_frame)
+
+        """ START OF REFRESH MODE VARIABLES """
+
+        # refresh checkbox
+        self.refresh_var = BooleanVar()
+        self.refresh_checkbox = Checkbutton(self.left_frame, text='Refresh Mode', variable=self.refresh_var)
+        self.refresh_checkbox.bind("<Button-1>", self.update_refresh_mode)
+        self.refresh_checkbox.pack(anchor='w')
+
+        # refresh amount input and label
+        self.refresh_amt_label = Label(self.left_frame, text='Enter Refresh Amount:')
+        self.refresh_amt_label.pack(anchor='w')
+        self.refresh_amt_input = Entry(self.left_frame)
+        self.refresh_amt_input.insert(0, 'Refresh Amount Here')
+        self.refresh_amt_input.pack(anchor='w')
+
+        # refresh delay input and label
+        self.refresh_dly_label = Label(self.left_frame, text='Enter Refresh Delay (Seconds):')
+        self.refresh_dly_label.pack(anchor='w')
+        self.refresh_dly_input = Entry(self.left_frame)
+        self.refresh_dly_input.insert(0, 'Refresh Delay Here')
+        self.refresh_dly_input.pack(anchor='w')
+
+        """  END OF REFRESH MODE VARIABLES  """
+        self.disable_refresh()
+
+        add_space(self.left_frame)
+
         self.submit_btn = Button(self.left_frame, text='Display Stock', command=self.get_stock)
         self.submit_btn.pack(anchor='w')
 
-        self.status_entry = Entry(self.left_frame)
+        add_space(self.left_frame)
+        add_space(self.left_frame)
+        add_space(self.left_frame)
+
+        self.status = Label(self.left_frame)
+        self.status.pack(anchor='w')
         self.update_status('Awaiting SKU...')
 
-        self.quit_btn = Button(self.left_frame, text='Quit', command=self.left_frame.quit)
+        self.quit_btn = Button(self.left_frame, text='Quit', command=master.quit)
         self.quit_btn.pack(anchor='w')
 
+    def update_refresh_mode(self, event=''):
+        # you must take the opposite since the function is called
+        # before the change of the checkbox
+        active = not self.refresh_var.get()
+
+        if active:
+            self.enable_refresh()
+        else:
+            self.disable_refresh()
+
+    def disable_refresh(self):
+        self.refresh_amt_label.configure(state="disabled")
+        self.refresh_amt_input.configure(state="disabled")
+        self.refresh_dly_label.configure(state="disabled")
+        self.refresh_dly_input.configure(state="disabled")
+
+    def enable_refresh(self):
+        self.refresh_amt_label.configure(state="normal")
+        self.refresh_amt_input.configure(state="normal")
+        self.refresh_dly_label.configure(state="normal")
+        self.refresh_dly_input.configure(state="normal")
+
     def update_status(self, status):
-        self.status_entry.configure(state=NORMAL)
-        self.status_entry.delete(0, 'end')
-        self.status_entry.insert(0, status)
-        self.status_entry.configure(state='readonly')
-        self.status_entry.pack()
+        self.status['text'] = 'Status: '+status
 
     def get_stock(self):
         self.sku = self.sku_input.get().strip()
         self.client_id = self.client_id_input.get().strip()
 
-        self.update_status('Retrieving {}...'.format(self.sku))
-
         region = self.region_input.get()
 
-        thread = Thread(name=self.sku+'_'+region, target=self.check_stock, args=[region])
+        if self.refresh_var.get():
+            validity = self.refresh_is_valid()
+            if not validity[0]:
+                self.update_status(validity[1])
+                return
+            else:
+                amt = int(self.refresh_amt_input.get())
+                dly = float(self.refresh_dly_input.get())
+        else:
+            amt = 1
+            dly = 1
+
+        thread = Thread(name=self.sku+'_'+region, target=self.check_stock, args=[region, amt, dly])
         thread.daemon = True
         thread.start()
 
-    def check_stock(self, region):
+    def check_stock(self, region, refresh=1, delay=1):
+        self.update_status('Retrieving {}...'.format(self.sku))
+
         region = region.upper()
         given_sku = self.sku
         is_main_sku = '_' not in self.sku
@@ -92,6 +170,7 @@ class StockCheckerApp(object):
 
         if is_main_sku:
             variants = json_dict['variants']
+            variant_attrs = json_dict['variation_attributes'][0]['values']
             for variant in variants:
                 index = variants.index(variant)
                 self.sku = variant['product_id']
@@ -110,6 +189,11 @@ class StockCheckerApp(object):
             self.update_status('Retrieved '+given_sku)
 
             self.display_stock(stock, json_dict['name'])
+
+            self.sku = given_sku
+            if refresh > 1:
+                time.sleep(delay)
+                self.check_stock(region, refresh-1)
         else:
             return qty
 
@@ -142,6 +226,29 @@ class StockCheckerApp(object):
             qty_display.insert(0, str(stock_entry[sku]))
             qty_display.configure(state='readonly')
             qty_display.grid(row=i, column=1)
+
+    def refresh_is_valid(self):
+        err_msg = ''
+        valid = True
+
+        try:
+            float(self.refresh_dly_input.get())
+        except ValueError:
+            err_msg = 'Please input valid refresh delay'
+            valid = False
+
+        try:
+            int(self.refresh_amt_input.get())
+        except ValueError:
+            err_msg = 'Please input valid refresh amount'
+            valid = False
+
+        return valid, err_msg
+
+
+def add_space(frame):
+    space = Label(frame)
+    space.pack()
 
 
 root = Tk()
